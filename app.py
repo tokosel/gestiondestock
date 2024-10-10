87c97f570bb0da60171d589648b51bd7acfedf58
tokosel <abdoulayesallboke@outlook.fr>
2024-10-10 22:03:02 +0000

    Déploiement

--- app.py
+++ app.py
@@ -732,4 +732,4 @@ def generate_report():
 # =================== Démarrage de l'application =====================
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=5000)
